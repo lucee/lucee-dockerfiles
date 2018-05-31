4a4c315c676b9a2afa39afb2da5b2c81a2599bfb
Kristofer Rye <kristofer.rye@gmail.com>
2018-05-31 16:16:06 -0500

    Delete most of the duplicated files

--- 5.2/prewarm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-/usr/local/tomcat/bin/catalina.sh start
-while [ ! -f "/opt/lucee/web/logs/application.log" ] ; do sleep 2; done && \
-/usr/local/tomcat/bin/catalina.sh stop
-rm -rf /opt/lucee/web/logs/*
