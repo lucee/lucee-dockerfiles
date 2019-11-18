cd88927e9fd9e8e5c8b9d1f5887278d66b99342f
Justin Carter <justin@daemon.com.au>
2019-11-18 11:52:40 +1100

    additional wait time for tomcat to stop during prewarm

--- supporting/prewarm.sh
+++ supporting/prewarm.sh
@@ -6,4 +6,5 @@ while [ ! -f "/opt/lucee/web/logs/application.log" ] ; do sleep 2; done
 while [ ! -d "/opt/lucee/server/lucee-server/deploy" ] ; do sleep 2; done
 sleep 1
 /usr/local/tomcat/bin/catalina.sh stop
+sleep 3
 rm -rf /opt/lucee/web/logs/*
